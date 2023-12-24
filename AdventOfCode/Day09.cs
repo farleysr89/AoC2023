e702ab40c1e8f6b567262ef26fbe571be5dd410f
Farley <33631912+farleysr89@users.noreply.github.com>
2023-12-24 08:29:58 -0600

    Day 9 Complete

---
+++ AdventOfCode/Day09.cs
@@ -0,0 +1,116 @@
+﻿using System.Net.Mail;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+
+namespace AdventOfCode;
+
+public class Day09 : BaseDay
+{
+    private readonly string _input;
+    private readonly string[] _inputs;
+    private static readonly char[] separator = [' '];
+
+    public Day09()
+    {
+        _input = File.ReadAllText(InputFilePath);
+        _inputs = _input.Split("\r\n");
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var total = 0;
+        foreach (var input in _inputs)
+        {
+            List<List<int>> values = [input.Split(separator).Select(x => int.Parse(x)).ToList()];
+            var currentList = values.First();
+            var first = true;
+            var prev = 0;
+            while (true)
+            {
+                List<int> differences = new List<int>();
+                foreach (var i in currentList)
+                {
+                    if (first)
+                    {
+                        prev = i;
+                        first = false;
+                        continue;
+                    }
+                    differences.Add(i - prev);
+                    prev = i;
+                }
+                values.Add(differences);
+                if(!values.Last().Any(x => x != 0)) break;
+                first = true;
+                currentList = values.Last();
+            }
+            values.Reverse();
+            first = true;
+            prev = 0;
+            foreach(var l in values)
+            {
+                if (first)
+                {
+                    prev = l.Last();
+                    first = false;
+                    continue;
+                }
+                prev += l.Last();
+            }
+            total += prev;
+        }
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is " + total);
+    }
+
+
+
+    public override ValueTask<string> Solve_2()
+    {
+        var total = 0;
+        foreach (var input in _inputs)
+        {
+            List<List<int>> values = [input.Split(separator).Select(x => int.Parse(x)).ToList()];
+            var currentList = values.First();
+            var first = true;
+            var prev = 0;
+            while (true)
+            {
+                List<int> differences = new List<int>();
+                foreach (var i in currentList)
+                {
+                    if (first)
+                    {
+                        prev = i;
+                        first = false;
+                        continue;
+                    }
+                    differences.Add(i - prev);
+                    prev = i;
+                }
+                values.Add(differences);
+                if(!values.Last().Any(x => x != 0)) break;
+                first = true;
+                currentList = values.Last();
+            }
+            values.Reverse();
+            first = true;
+            prev = 0;
+            foreach(var l in values)
+            {
+                if (first)
+                {
+                    prev = l.First();
+                    first = false;
+                    continue;
+                }
+                prev = l.First() - prev;
+            }
+            total += prev;
+        }
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + total);
+    }
+
+}
