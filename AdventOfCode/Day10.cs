18bd34b17e211d55fe16ed553db9ebf4a45600db
Farley <33631912+farleysr89@users.noreply.github.com>
2023-12-26 20:21:12 -0600

    Day 10 Complete

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -1,4 +1,7 @@
-﻿namespace AdventOfCode;
+﻿using Spectre.Console.Rendering;
+using System.Net.WebSockets;
+
+namespace AdventOfCode;
 
 public class Day10 : BaseDay
 {
@@ -339,7 +342,7 @@ public class Day10 : BaseDay
     {
         x++;
         if (x >= map[y].Length) return false;
-        if (map[y][x] != '.')
+        if (map[y][x] != '.' && map[y][x] != 'L' && map[y][x] != 'F')
             return true;
         return false;
     }
@@ -348,7 +351,7 @@ public class Day10 : BaseDay
     {
         x--;
         if (x < 0) return false;
-        if (map[y][x] != '.')
+        if (map[y][x] != '.' && map[y][x] != '7' && map[y][x] != 'J')
             return true;
         return false;
     }
@@ -357,7 +360,7 @@ public class Day10 : BaseDay
     {
         y++;
         if (y >= map.Length) return false;
-        if (map[y][x] != '.')
+        if (map[y][x] != '.' && map[y][x] != 'F' && map[y][x] != '7')
             return true;
         return false;
     }
@@ -366,16 +369,360 @@ public class Day10 : BaseDay
     {
         y--;
         if (y < 0) return false;
-        if (map[y][x] != '.')
+        if (map[y][x] != '.' && map[y][x] != 'L' && map[y][x] != 'J')
             return true;
         return false;
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var total = 0;
+var total = 0;
+        var map = new char[_inputs.Length][];
+        var count = 0;
+        foreach (var input in _inputs)
+        {
+            map[count] = input.ToCharArray();
+            count++;
+        }
+        (int, int) start = (-1, -1);
+        var y = 0;
+        foreach (var l in map)
+        {
+            int x = 0;
+            foreach (var c in l)
+            {
+                if (c == 'S')
+                {
+                    start = (x, y);
+                    break;
+                }
+                x++;
+            }
+            if (start != (-1, -1)) break;
+            y++;
+        }
+        var steps = new int[map.Length][];
+        for (int i = 0; i < map.Length; i++)
+        {
+            steps[i] = new int[map[i].Length];
+        }
+        for (int i = 0; i < steps.Length; i++)
+        {
+            for (int j = 0; j < steps[i].Length; j++)
+            {
+                steps[i][j] = int.MaxValue;
+            }
+        }
+        steps[start.Item2][start.Item1] = 0;
+        if (TryRight(start.Item1, start.Item2, map))
+        {
+            if (TryUp(start.Item1, start.Item2, map))
+                map[start.Item2][start.Item1] = 'L';
+            else if (TryDown(start.Item1, start.Item2, map))
+                map[start.Item2][start.Item1] = 'F';
+        }
+        else if (TryLeft(start.Item1, start.Item2, map))
+        {
+            if (TryUp(start.Item1, start.Item2, map))
+                map[start.Item2][start.Item1] = 'J';
+            else if (TryDown(start.Item1, start.Item2, map))
+                map[start.Item2][start.Item1] = '7';
+        }
+        (int, int) next = (-1, -1);
+        (int, int) prev = start;
+        char symbol = map[start.Item2][start.Item1];
+        char prevSymbol;
+        char dir = '.';
+        if (symbol == 'F') { next = (start.Item1 + 1, start.Item2); dir = 'R'; }
+        if (symbol == 'L') { next = (start.Item1 + 1, start.Item2); dir = 'R'; }
+        if (symbol == 'J') { next = (start.Item1 - 1, start.Item2); dir = 'L'; }
+        if (symbol == '7') { next = (start.Item1 - 1, start.Item2); dir = 'L'; }
+        count = 0;
+        var area = 0;
+        while (next != start)
+        {
+            count++;
+            prevSymbol = symbol;
+            symbol = map[next.Item2][next.Item1];
+            steps[next.Item2][next.Item1] = Math.Min(count, steps[next.Item2][next.Item1]);
+            
+            if (symbol == '.')
+            {
+                Console.WriteLine("Something Broke!"); break;
+            }
+            else if (symbol == '-')
+            {
+                if (dir == 'R')
+                {
+                    next = (++next.Item1, next.Item2);
+                }
+                else if (dir == 'L')
+                {
+                    next = (--next.Item1, next.Item2);
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == '|')
+            {
+                if (dir == 'U')
+                {
+                    next = (next.Item1, --next.Item2);
+                }
+                else if (dir == 'D')
+                {
+                    next = (next.Item1, ++next.Item2);
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == 'F')
+            {
+                if (dir == 'U')
+                {
+                    next = (++next.Item1, next.Item2);
+                    dir = 'R';
+                }
+                else if (dir == 'L')
+                {
+                    next = (next.Item1, ++next.Item2);
+                    dir = 'D';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == 'J')
+            {
+                if (dir == 'R')
+                {
+                    next = (next.Item1, --next.Item2);
+                    dir = 'U';
+                }
+                else if (dir == 'D')
+                {
+                    next = (--next.Item1, next.Item2);
+                    dir = 'L';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == 'L')
+            {
+                if (dir == 'D')
+                {
+                    next = (++next.Item1, next.Item2);
+                    dir = 'R';
+                }
+                else if (dir == 'L')
+                {
+                    next = (next.Item1, --next.Item2);
+                    dir = 'U';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == '7')
+            {
+                if (dir == 'R')
+                {
+                    next = (next.Item1, ++next.Item2);
+                    dir = 'D';
+                }
+                else if (dir == 'U')
+                {
+                    next = (--next.Item1, next.Item2);
+                    dir = 'L';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something Broke!");
+                break;
+            }
+            area += ((prev.Item1 * next.Item2) - (next.Item1 * prev.Item2));
+            prev = next;
+        }
+        area += ((prev.Item1 * next.Item2) - (next.Item1 * prev.Item2));
+        area = Math.Abs( area) / 2;
 
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + total);
+        
+        symbol = map[start.Item2][start.Item1];        
+        dir = '.';
+        if (symbol == 'F') { next = (start.Item1, start.Item2 + 1); dir = 'D'; }
+        if (symbol == 'L') { next = (start.Item1, start.Item2 - 1); dir = 'U'; }
+        if (symbol == 'J') { next = (start.Item1, start.Item2 + 1); dir = 'U'; }
+        if (symbol == '7') { next = (start.Item1, start.Item2 - 1); dir = 'D'; }
+        count = 0;
+        while (next != start)
+        {
+            count++;
+            prevSymbol = symbol;
+            symbol = map[next.Item2][next.Item1];
+            steps[next.Item2][next.Item1] = Math.Min(count, steps[next.Item2][next.Item1]);
+            prev = next;
+            if (symbol == '.')
+            {
+                Console.WriteLine("Something Broke!"); break;
+            }
+            else if (symbol == '-')
+            {
+                if (dir == 'R')
+                {
+                    next = (++next.Item1, next.Item2);
+                }
+                else if (dir == 'L')
+                {
+                    next = (--next.Item1, next.Item2);
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == '|')
+            {
+                if (dir == 'U')
+                {
+                    next = (next.Item1, --next.Item2);
+                }
+                else if (dir == 'D')
+                {
+                    next = (next.Item1, ++next.Item2);
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == 'F')
+            {
+                if (dir == 'U')
+                {
+                    next = (++next.Item1, next.Item2);
+                    dir = 'R';
+                }
+                else if (dir == 'L')
+                {
+                    next = (next.Item1, ++next.Item2);
+                    dir = 'D';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == 'J')
+            {
+                if (dir == 'R')
+                {
+                    next = (next.Item1, --next.Item2);
+                    dir = 'U';
+                }
+                else if (dir == 'D')
+                {
+                    next = (--next.Item1, next.Item2);
+                    dir = 'L';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == 'L')
+            {
+                if (dir == 'D')
+                {
+                    next = (++next.Item1, next.Item2);
+                    dir = 'R';
+                }
+                else if (dir == 'L')
+                {
+                    next = (next.Item1, --next.Item2);
+                    dir = 'U';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else if (symbol == '7')
+            {
+                if (dir == 'R')
+                {
+                    next = (next.Item1, ++next.Item2);
+                    dir = 'D';
+                }
+                else if (dir == 'U')
+                {
+                    next = (--next.Item1, next.Item2);
+                    dir = 'L';
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something Broke!");
+                break;
+            }
+        }
+        foreach (var l in steps)
+        {
+            foreach (var s in l)
+            {
+                if (s != int.MaxValue && s > total) total = s;
+            }
+        }
+        var interiorPoints = area + 1 - (total);
+        bool outOfLoop = true; bool inPipe = false;
+        //foreach(var line in steps)
+        //{
+        //    foreach(var s in line)
+        //    {
+        //        if(s != int.MaxValue) { outOfLoop = !outOfLoop; continue;}
+        //        if(!outOfLoop) total++;
+        //        inPipe = false;
+                
+        //    }
+        //}
+        //for(int yy = 0;yy < steps.Count(); yy++)
+        //{
+        //    for(int x = 0; x < steps[yy].Count(); x++)
+        //    {
+        //        if(steps[yy][x] != int.MaxValue) { outOfLoop = !outOfLoop; continue;}
+        //        if(!outOfLoop) total++;
+        //        inPipe = false;
+        //    }
+        //}
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + interiorPoints);
     }
 
 }
